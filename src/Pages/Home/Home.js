a1eed1700537cdf6441520aa33f630cdc74ea769
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2023-07-06 15:55:55 +0500

    add latest event work daniy

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,12 +3,14 @@ import Layout from "../../Layout"
 import Carousel from '../../components/Carousel/Carousel'
 import Latestevents from '../../components/LatestEvent/Latestevents'
 import Sponser from '../../components/Sponser/Sponser'
+import Teampictures from '../../components/Teampictures/Teampictures'
 
 const Home = () => {
     return (
         <Layout>
             <Carousel />
             <Latestevents />
+            <Teampictures />
             <Sponser />
         </Layout>
     )
---
+++ src/components/Teampictures/Teampictures.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import './Teampictures.css'
+
+const Teampictures = () => {
+    return (
+        <>
+            <div className="container-fluid my-5">
+                <div className="container">
+                    <h1 className='myheading fw-bold team-heading'>Team Pictures</h1>
+                </div>
+                <div className="row">
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="https://preview.colorlib.com/theme/evento/assets/img/speakers/s1.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="	https://preview.colorlib.com/theme/evento/assets/img/speakers/s2.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="https://preview.colorlib.com/theme/evento/assets/img/speakers/s3.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="https://preview.colorlib.com/theme/evento/assets/img/speakers/s4.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="https://preview.colorlib.com/theme/evento/assets/img/speakers/s1.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="https://preview.colorlib.com/theme/evento/assets/img/speakers/s2.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="https://preview.colorlib.com/theme/evento/assets/img/speakers/s3.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3 p-0">
+                        <div className='team-card'>
+                            <img src="https://preview.colorlib.com/theme/evento/assets/img/speakers/s4.png.webp" alt="team" width="100%" height="100%" />
+                            <div className='team-card-inner'>
+                                <h5 className='team-member-name'>Daniy</h5>
+                                <p className='team-member-pos'>CEO DanaSoft</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Teampictures
