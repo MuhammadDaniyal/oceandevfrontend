e16bb28825cb35137932e0282baa2d15b4053d65
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2023-07-06 13:26:55 +0500

    add latest event work daniy

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Layout from "../../Layout"
 import Carousel from '../../components/Carousel/Carousel'
 import Latestevents from '../../components/LatestEvent/Latestevents'
+import Sponser from '../../components/Sponser/Sponser'
 
 const Home = () => {
     return (
         <Layout>
-            <Carousel/>
-            <Latestevents/>
+            <Carousel />
+            <Latestevents />
+            <Sponser />
         </Layout>
     )
 }
---
+++ src/components/Sponser/Sponser.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import './Sponser.css'
+import Slider from "react-slick";
+
+const Sponser = () => {
+    const settings = {
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                setting: {
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 520,
+                setting: {
+                    slidesToShow: 3,
+                },
+            },
+        ],
+    };
+    return (
+        <>
+
+
+            <section className="container-fluid section-sponser my-5">
+                <div className='container'>
+                    <h1 className='myheading fw-bold'>Our Sponsers</h1>
+                </div>
+                <Slider className="customer-logos slider" {...settings}>
+
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+
+
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+                    <div className="slide"><img src="./images/spons.png" alt="logo" /></div>
+
+
+                </Slider>
+            </section>
+        </>
+
+    )
+}
+
+export default Sponser
